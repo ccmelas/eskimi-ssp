48e1d3e5816fab4f82cd9c4f6ecbca8e6c772353
Chiemela Chinedum <chiemelachinedum@gmail.com>
2021-10-17 21:21:40 +0100

    Adds application routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CampaignController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('campaigns', [CampaignController::class, 'index']);
+Route::post('campaigns', [CampaignController::class, 'store']);
+// post used for update because of files
+Route::post('campaigns/{campaign}', [CampaignController::class, 'update']);
